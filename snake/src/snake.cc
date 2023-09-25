7aff8fce5b83a9bced72f25d084a06084c871fbf
semperesa <semperesa@esat-libertad.com>
2023-09-25 19:03:53 +0200

    Added window grid

--- snake/src/snake.cc
+++ snake/src/snake.cc
@@ -9,28 +9,40 @@
 #include <esat/input.h>
 #include <esat/time.h>
 
+const int kQuadSize = 32;
+const int kWindowWidth = 640;
+const int kWindowHeight = 480;
+
 struct Quad {
-  int x1, x2, y1, y2;
+  int x1, y1, x2, y2;
 };
 
 struct Pos {
   float x, y;
 };
 
+void DrawTiles() {
+  for (int x = kQuadSize; x < kWindowWidth; x += kQuadSize) {
+    esat::DrawSetStrokeColor(255, 255, 255, 255);
+    esat::DrawLine(x, 0, x, kWindowHeight);
+  }
+  for (int y = kQuadSize; y < kWindowHeight; y += kQuadSize) {
+    esat::DrawSetStrokeColor(255, 255, 255, 255);
+    esat::DrawLine(0, y, kWindowWidth, y);
+  }
+}
+
 Pos MoveSnake(Pos pos) {
 
   pos.x += 32.0f;
-
-
-
   
   return pos;
 }
 
 Quad GenerateRandomQuad() {
-  int randx = (rand() % 40) * 40;
-  int randy = (rand() % 30) * 30;
-  Quad quad = { (float) randx, (float) (randx + 32), (float) randy, (float) (randy + 32)};
+  int randx = (rand() % 20) * kQuadSize;
+  int randy = (rand() % 15) * kQuadSize;
+  Quad quad = { (float) randx, (float) (randx + kQuadSize), (float) randy, (float) (randy + kQuadSize)};
   return quad;
 }
 
@@ -48,6 +60,9 @@ void DrawQuad(Quad quad) {
 }
 
 void Snake() {
+
+  srand(time(NULL));
+
   esat::WindowInit(640, 480);
   esat::WindowSetMouseVisibility(true);
 
@@ -61,10 +76,13 @@ void Snake() {
     esat::DrawBegin();
     esat::DrawClear(0, 0, 0);
     
+    //Update
     pos = MoveSnake(pos);
 
+    //Draw
     esat::DrawSprite(snake_sprite, pos.x, pos.y);
     DrawQuad(quad);
+    DrawTiles();
 
     esat::DrawEnd();
 
